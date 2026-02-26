100fb7047df8dd4e7a8261ddd3a1beb7682048bf
HuyAtreides <huyatreides@gmail.com>
2026-02-26 10:50:24 +0700

    neovim: remove java lang and lualine

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -5,6 +5,12 @@ return {
       picker = {
         hidden = true, -- Shows hidden files
         ignored = false, -- Shows files ignored by .gitignore
+        sources = {
+          explorer = {
+            git_status = false, -- Disable git icons in the explorer list
+            git_untracked = false,
+          },
+        },
       },
     },
   },
